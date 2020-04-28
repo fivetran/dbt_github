abab4a20854b87df8055019795705b9c10178f5c
Erik Magnuson <erikm@fivetran.com>
2020-04-28 15:48:25 -0700

    adding transform

---
+++ models/transform/intermediate/issue_open_length.sql
@@ -0,0 +1,13 @@
+with issue_close_stack as (
+
+    select *
+    from {{ ref('issue_close_stack') }}
+  
+)
+
+select
+  issue_id,
+  sum(timestamp_diff(least(valid_until, current_timestamp()), valid_starting, second)/86400) as days_issue_opened
+from issue_close_stack
+  where not is_closed
+group by issue_id
