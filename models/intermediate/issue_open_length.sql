2e9b5e1fdda1b2c07b9e5c7e95edcf677a7307f9
Erik Magnuson <erikm@fivetran.com>
2020-05-07 14:56:43 -0700

    adding descriptions and removing unnecessary fields

--- models/intermediate/issue_open_length.sql
+++ models/intermediate/issue_open_length.sql
@@ -7,7 +7,7 @@ with issue_close_stack as (
 
 select
   issue_id,
-  sum(timestamp_diff(least(valid_until, current_timestamp()), valid_starting, second)/86400) as days_issue_opened
+  sum(timestamp_diff(least(valid_until, current_timestamp()), valid_starting, second)/86400) as days_issue_open
 from issue_close_stack
   where not is_closed
 group by issue_id
