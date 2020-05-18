21137194a13900a1b86833f6792435c7d2945c84
fivetran-kristin <kristin@fivetran.com>
2020-05-18 10:34:43 -0700

    round 1 changes

--- models/intermediate/pull_request_times.sql
+++ models/intermediate/pull_request_times.sql
@@ -50,11 +50,19 @@ with pull_request_review as (
 select
   first_request_time.issue_id,
   issue_merged.merged_at,
-  {{ dbt_utils.datediff('time_of_first_request', "coalesce(time_of_first_review_post_request, current_timestamp)", 'second') }}/3600 as hours_request_review_to_first_review,
-  {{ dbt_utils.datediff('time_of_first_request', 
-                        "least(coalesce(time_of_first_requested_reviewer_review, current_timestamp()),
-                              coalesce(issue.closed_at, current_timestamp()))", 'second') }}/3600 as hours_request_review_to_first_action,
-  {{ dbt_utils.datediff('time_of_first_request', 'merged_at', 'second') }}/3600 as hours_request_review_to_merge
+  {{ dbt_utils.datediff(
+                        'time_of_first_request', 
+                        "coalesce(time_of_first_review_post_request, " ~ dbt_utils.current_timestamp() ~ ")", 
+                        'second') 
+  }}/ 60/60 as hours_request_review_to_first_review,
+  {{ dbt_utils.datediff(
+                        'time_of_first_request', 
+                        "least(
+                            coalesce(time_of_first_requested_reviewer_review, " ~ dbt_utils.current_timestamp() ~ "),
+                            coalesce(issue.closed_at, " ~ dbt_utils.current_timestamp() ~ "))", 
+                        'second') 
+  }} / 60/60 as hours_request_review_to_first_action,
+  {{ dbt_utils.datediff('time_of_first_request', 'merged_at', 'second') }}/ 60/60 as hours_request_review_to_merge
 from first_request_time
 join issue on first_request_time.issue_id = issue.issue_id
 left join issue_merged on first_request_time.issue_id = issue_merged.issue_id
