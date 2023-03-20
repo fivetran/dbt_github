3fbd9c7609d04aae78b827ebe2c7eb5add7e6147
fivetran-sheringuyen <94874400+fivetran-sheringuyen@users.noreply.github.com>
2023-03-20 10:26:44 -0800

    dbt utils 1.0 update

--- models/github__monthly_metrics.sql
+++ models/github__monthly_metrics.sql
@@ -4,7 +4,7 @@ with daily_metrics as (
 )
 
 select 
-  {{ dbt_utils.date_trunc('month', 'day') }} as month, 
+  {{ dbt.date_trunc('month', 'day') }} as month, 
   sum(number_issues_opened) as number_issues_opened,
   sum(number_issues_closed) as number_issues_closed,
   sum(sum_days_issue_open) / sum(number_issues_opened) as avg_days_issue_open,
--- models/github__weekly_metrics.sql
+++ models/github__weekly_metrics.sql
@@ -4,7 +4,7 @@ with daily_metrics as (
 )
 
 select 
-  {{ dbt_utils.date_trunc('week', 'day') }} as week, 
+  {{ dbt.date_trunc('week', 'day') }} as week, 
   sum(number_issues_opened) as number_issues_opened,
   sum(number_issues_closed) as number_issues_closed,
   sum(sum_days_issue_open) / sum(number_issues_opened) as avg_days_issue_open,
