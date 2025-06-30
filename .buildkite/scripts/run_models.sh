0a23b4a3314835940a81e78b3fe5eebfe688c812
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2025-06-30 14:10:13 -0400

    tests

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -19,7 +19,7 @@ dbt deps
 dbt seed --target "$db" --full-refresh
 dbt source freshness --target "$db" || echo "...Only verifying freshness runs…"
 dbt run --vars '{github__using_repo_team: false, github__using_issue_assignee: false, github__using_issue_label: false, github__using_requested_reviewer_history: false}' --target "$db" --full-refresh
-dbt test --target "$db"
+dbt test --vars '{github__using_repo_team: false, github__using_issue_assignee: false, github__using_issue_label: false, github__using_requested_reviewer_history: false}' --target "$db"
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
 
--- integration_tests/tests/consistency_monthly_metrics.sql
+++ integration_tests/tests/consistency_monthly_metrics.sql
@@ -8,65 +8,76 @@ with prod as (
     select 
         month,
         repository,
-        round(number_issues_opened, 0) as number_issues_opened,
-        round(number_issues_closed, 0) as number_issues_closed,
-        round(avg_days_issue_open, 0) as avg_days_issue_open,
-        round(longest_days_issue_open, 0) as longest_days_issue_open,
-        round(number_prs_opened, 0) as number_prs_opened,
-        round(number_prs_merged, 0) as number_prs_merged,
-        round(number_prs_closed_without_merge, 0) as number_prs_closed_without_merge,
-        round(avg_days_pr_open, 0) as avg_days_pr_open,
-        round(longest_days_pr_open, 0) as longest_days_pr_open
+        sum(coalesce(number_issues_opened, 0)) as number_issues_opened,
+        sum(coalesce(number_issues_closed, 0)) as number_issues_closed,
+        sum(coalesce(avg_days_issue_open, 0)) as avg_days_issue_open,
+        sum(coalesce(longest_days_issue_open, 0)) as longest_days_issue_open,
+        sum(coalesce(number_prs_opened, 0)) as number_prs_opened,
+        sum(coalesce(number_prs_merged, 0)) as number_prs_merged,
+        sum(coalesce(number_prs_closed_without_merge, 0)) as number_prs_closed_without_merge,
+        sum(coalesce(avg_days_pr_open, 0)) as avg_days_pr_open,
+        sum(coalesce(longest_days_pr_open, 0)) as longest_days_pr_open
 
     from {{ target.schema }}_github_prod.github__monthly_metrics
     where date(month) < current_date() -- BQ 
+    group by 1, 2
 ),
 
 dev as (
-    select
+    select 
         month,
         repository,
-        round(number_issues_opened, 0) as number_issues_opened,
-        round(number_issues_closed, 0) as number_issues_closed,
-        round(avg_days_issue_open, 0) as avg_days_issue_open,
-        round(longest_days_issue_open, 0) as longest_days_issue_open,
-        round(number_prs_opened, 0) as number_prs_opened,
-        round(number_prs_merged, 0) as number_prs_merged,
-        round(number_prs_closed_without_merge, 0) as number_prs_closed_without_merge,
-        round(avg_days_pr_open, 0) as avg_days_pr_open,
-        round(longest_days_pr_open, 0) as longest_days_pr_open
+        sum(coalesce(number_issues_opened, 0)) as number_issues_opened,
+        sum(coalesce(number_issues_closed, 0)) as number_issues_closed,
+        sum(coalesce(avg_days_issue_open, 0)) as avg_days_issue_open,
+        sum(coalesce(longest_days_issue_open, 0)) as longest_days_issue_open,
+        sum(coalesce(number_prs_opened, 0)) as number_prs_opened,
+        sum(coalesce(number_prs_merged, 0)) as number_prs_merged,
+        sum(coalesce(number_prs_closed_without_merge, 0)) as number_prs_closed_without_merge,
+        sum(coalesce(avg_days_pr_open, 0)) as avg_days_pr_open,
+        sum(coalesce(longest_days_pr_open, 0)) as longest_days_pr_open
 
     from {{ target.schema }}_github_dev.github__monthly_metrics
     where date(month) < current_date() -- BQ
-),
-
-prod_not_in_dev as (
-    -- rows from prod not found in dev
-    select * from prod
-    except distinct
-    select * from dev
-),
-
-dev_not_in_prod as (
-    -- rows from dev not found in prod
-    select * from dev
-    except distinct
-    select * from prod
+    group by 1, 2
 ),
 
 final as (
-    select
-        *,
-        'from prod' as source
-    from prod_not_in_dev
-
-    union all -- union since we only care if rows are produced
-
-    select
-        *,
-        'from dev' as source
-    from dev_not_in_prod
+    select 
+        coalesce(prod.month, dev.month) as month,
+        prod.number_issues_opened as prod_number_issues_opened,
+        dev.number_issues_opened as dev_number_issues_opened,
+        prod.number_issues_closed as prod_number_issues_closed,
+        dev.number_issues_closed as dev_number_issues_closed,
+        prod.avg_days_issue_open as prod_avg_days_issue_open,
+        dev.avg_days_issue_open as dev_avg_days_issue_open,
+        prod.longest_days_issue_open as prod_longest_days_issue_open,
+        dev.longest_days_issue_open as dev_longest_days_issue_open,
+        prod.number_prs_opened as prod_number_prs_opened,
+        dev.number_prs_opened as dev_number_prs_opened,
+        prod.number_prs_merged as prod_number_prs_merged,
+        dev.number_prs_merged as dev_number_prs_merged,
+        prod.number_prs_closed_without_merge as prod_number_prs_closed_without_merge,
+        dev.number_prs_closed_without_merge as dev_number_prs_closed_without_merge,
+        prod.avg_days_pr_open as prod_avg_days_pr_open,
+        dev.avg_days_pr_open as dev_avg_days_pr_open,
+        prod.longest_days_pr_open as prod_longest_days_pr_open,
+        dev.longest_days_pr_open as dev_longest_days_pr_open
+    from prod
+    full outer join dev 
+        on dev.repository = prod.repository
+        and dev.month = prod.month
 )
 
 select *
 from final
+where
+    abs(prod_number_issues_opened - dev_number_issues_opened) >= .01
+    or abs(prod_number_issues_closed - dev_number_issues_closed) >= .01
+    or abs(prod_avg_days_issue_open - dev_avg_days_issue_open) >= .01
+    or abs(prod_longest_days_issue_open - dev_longest_days_issue_open) >= .01
+    or abs(prod_number_prs_opened - dev_number_prs_opened) >= .01
+    or abs(prod_number_prs_merged - dev_number_prs_merged) >= .01
+    or abs(prod_number_prs_closed_without_merge - dev_number_prs_closed_without_merge) >= .01
+    or abs(prod_avg_days_pr_open - dev_avg_days_pr_open) >= .01
+    or abs(prod_longest_days_pr_open - dev_longest_days_pr_open) >= .01
