5efbf7ad43360fdd7dfbd6fcfcd0c3e6f8227020
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-10-22 12:20:20 -0500

    update github_union_connections

--- macros/union/github_union_connections.sql
+++ macros/union/github_union_connections.sql
@@ -8,8 +8,10 @@
 
 {%- set exception_warning = "\n\nPlease be aware: The " ~ single_source_name|upper ~ "." ~ single_table_name|upper ~ " table was not found in your schema(s). The Fivetran Data Model will create a completely empty staging model as to not break downstream transformations. To turn off these warnings, set the `fivetran__remove_empty_table_warnings` variable to TRUE (see https://github.com/fivetran/dbt_fivetran_utils/tree/releases/v0.4.latest#union_data-source for details).\n"%}
 {%- set using_empty_table_warnings = (execute and not var('fivetran__remove_empty_table_warnings', false)) %}
+{%- set connections = var(connection_dictionary, []) %}
+{%- set using_unioning = connections | length > 0 %}
 
-{%- if connection_dictionary %}
+{%- if using_unioning %}
 {# For unioning #}
     {%- set relations = [] -%}
     {%- for connection in connection_dictionary -%}
--- models/staging/tmp/stg_github__user_tmp.sql
+++ models/staging/tmp/stg_github__user_tmp.sql
@@ -1,6 +1,6 @@
 {{
     github.github_union_connections(
-        connection_dictionary=var('github_sources'),
+        connection_dictionary='github_sources',
         single_source_name='github',
         single_table_name='user'
     )
