a5f4825de73cee3fdf4263522e79aa57ddff5f3d
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-10-27 12:19:57 -0500

    formatting

--- macros/union/github_union_connections.sql
+++ macros/union/github_union_connections.sql
@@ -41,7 +41,7 @@
     {{ '-- full-identifier_var: ' ~ identifier_var }}
     {{ '-- database: ' ~ database }}
     {{ '-- schema: ' ~ schema }}
-    {{ '-- identifier: ' ~ identifier }}
+    {{ '-- identifier: ' ~ identifier ~ '\n' }}
 
     {%- endfor -%}
 
@@ -75,7 +75,7 @@
     {{ '-- full-identifier_var: ' ~ identifier_var }}
     {{ '-- database: ' ~ database }}                            
     {{ '-- schema: ' ~ schema }}
-    {{ '-- identifier: ' ~ identifier }}
+    {{ '-- identifier: ' ~ identifier ~ '\n' }}
 
     {% if relation is not none -%}
         select
